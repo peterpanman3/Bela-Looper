2cb41766224cf52b0f1028eefac45c203295b191
peterpanman3 <47373770+peterpanman3@users.noreply.github.com>
2019-03-04 22:09:59 -0500

    Update render (1).cpp
    Did some optimization/edited for better programming practices

--- render (1).cpp
+++ render (1).cpp
@@ -3,16 +3,16 @@
 #include <Scope.h>
 #include <cmath>
 
-#define LOOP_BUFFER_SIZE 882000  //20 seconds worth of looping
+#define LOOP_BUFFER_SIZE 882000  //20 seconds worth of looping (sampled at 44100 Hz)
 
-float gLoopBuffer[LOOP_BUFFER_SIZE] = {0};	 //Looper buffer
-int gLoopBufferPointer = 0;
-int gAudioFramesPerAnalogFrame = 0;
+unsigned int gAudioFramesPerAnalogFrame = 0;
 float gInverseSampleRate;
-int loopLength = 0;   //Used to shorten buffer to length of recorded loop
-float linGain = 1;
-float gain = 1;
-float shape = 3;
+float gLoopBuffer[LOOP_BUFFER_SIZE] = {0.0};	//Looper buffer
+static unsigned int gLoopBufferPointer = 0;
+static unsigned int loopLength = 0;   		//Used to shorten buffer to length of recorded loop
+float linGain = 1.0;				//Value read from analog input (linearly increases from 0-1 for linear pots)
+float gain = 1.0;				//Will be converted to exponential (dB) increase
+float shape = 3.0;				//Controls knee point of dB volume increase
 
 int gRecordPin = 0;
 int gVolumeKnob = 0;
@@ -32,12 +32,14 @@ void render(BelaContext *context, void *userData)
 {
 	
 	int control = digitalRead(context, 0, gRecordPin);
+	float in = audioRead(context,n,0);
+	float out = 0.0;
 		
 		
 
-	if (control != lastControl)
-		{
-			if (control)
+	if (control != lastControl)		//Detects pushbutton press
+		{				//NOTE: Not debounced, was developed as a proof of concept
+			if (control)		//For implementation, a debouncer should be used
 			{
 				m++;
 				rt_printf("m = %i\n", m);
@@ -51,16 +53,12 @@ void render(BelaContext *context, void *userData)
 	for (unsigned int n = 0; n < context->audioFrames; n++)
 	{
 		
-		float in = audioRead(context,n,0);
-		float out = 0;
-		
-		
 		if(!(n % gAudioFramesPerAnalogFrame))
 		{
 			linGain = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gVolumeKnob), 0.0, 1.0, 0.1, 1.0);
+			gain = (std::exp(linGain*shape)-1)/(std::exp(shape)-1);
 		}
 		
-		gain = (std::exp(linGain*shape)-1)/(std::exp(shape)-1);
 		
 		switch (m)
 		{
@@ -79,7 +77,7 @@ void render(BelaContext *context, void *userData)
 				if(++gLoopBufferPointer>=loopLength)
 					gLoopBufferPointer = 0;
 				out = in + gLoopBuffer[gLoopBufferPointer]*gain;
-				gLoopBufferPointer += in;
+				gLoopBuffer[gLoopBufferPointer] += in;
 				break; 
 				
 			case 3:				//Outputting complete loop without recording anymore
@@ -91,6 +89,10 @@ void render(BelaContext *context, void *userData)
 			case 4:				//Resets buffer length and pointer (doesn't clear buffer)
 				loopLength = 0;
 				gLoopBufferPointer = 0;
+				m = 0;
+				break;
+				
+			default:
 				m = 0;
 				break;
 		}
